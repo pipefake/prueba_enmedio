feb563199ebaef0c7809167783672c76a29723a0
Felipe Jimenez <jfjimenezsalazar@gmail.com>
2025-06-12 16:36:32 -0500

    Se crea la API para el login de los usuarios, con la documentación en swagger

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,12 +1,34 @@
 // src/routes/user-router.ts
 import express from "express";
-import { testUser } from "../controllers/user-controller";
-
+import { UserController } from "../controllers/user-controller";
+const userController = new UserController();
 //Inicializar el enrutador de Express
 const router = express.Router();
-
-//Definir la ruta de prueba para el usuario
-router.get('/', testUser);
+/**
+ * @swagger
+ * /api/user/login:
+ *   post:
+ *     summary: Iniciar sesión de usuario
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: jfjimenezsalazar@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: contraseña
+ *     responses:
+ *       200:
+ *         description: Inicio de sesión exitoso
+ *       401:
+ *         description: Credenciales incorrectas
+ */
+router.post('/login', userController.login); // ruta para el login
 
 //Exportar el enrutador
 export default router;
